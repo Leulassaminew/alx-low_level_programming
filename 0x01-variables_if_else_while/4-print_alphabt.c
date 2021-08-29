4cd43765ec39683fb5953a93f70a6015ac36824b
Leul Assaminew <leulassaminew@gmail.com>
2021-08-29 13:08:24 -0400

    updated

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,14 @@ int main(void)
 {
 char ch = 'a';
 
-while (ch <= 'z'){
+while (ch <= 'z')
+{
 if (ch != 'e' && ch != 'q')
 {
 putchar (ch);
 }
 ch++;
-putchar ('\n');
 }
+putchar ('\n');
 return (0);
 }
