439d28e4e7a173676c54dbac73d90374a30d1f90
Leul Assaminew <leulassaminew@gmail.com>
2021-08-29 13:14:26 -0400

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+printf ("%d" ,i);
+}
+printf ('\n');
+return (0);
+}
