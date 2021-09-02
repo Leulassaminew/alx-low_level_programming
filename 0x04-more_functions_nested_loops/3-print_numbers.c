043e34e01a43a45aa0aa6ee7d903233f54803c62
Leul Assaminew <leulassaminew@gmail.com>
2021-09-02 15:12:20 -0400

    updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
+ */
+void print_numbers(void)
+{
+int i;
+for(i = 0;i <= 9;i++){
+_putchar(i + '0');
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/alx/main.c
+++ 0x04-more_functions_nested_loops/alx/main.c
@@ -4,10 +4,14 @@ int _putchar(char c);
 int _islower(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 int main()
 {
 _islower(78);
 _isdigit(49);
 printf("%d mul is", mul(3,4));
+printf("\n");
+print_numbers();
+
 }
