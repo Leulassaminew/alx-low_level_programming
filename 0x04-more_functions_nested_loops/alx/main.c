b380351bfceb47c54fd0f68a377c247a9b6e3239
Leul Assaminew <leulassaminew@gmail.com>
2021-09-02 13:59:47 -0400

    updated

---
+++ 0x04-more_functions_nested_loops/alx/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <main.h>
+int _putchar(char c);
+int _islower(int c);
+int main()
+{
+    printf("this statement is from the main program");
+    _putchar(h);
+    _islower(78);
+
+}
