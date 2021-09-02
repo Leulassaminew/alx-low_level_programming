cefe208c89e52fe730e746292c674c150d72a8dd
Leul Assaminew <leulassaminew@gmail.com>
2021-09-02 16:45:36 -0400

    updated

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -4,10 +4,26 @@ int _putchar(char c);
 int _islower(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 int main()
 {
 _islower(78);
 _isdigit(49);
 printf("%d mul is", mul(3,4));
+printf("\n");
+print_numbers();
+print_most_numbers();
+more_numbers();
+print_line(5);
+print_diagonal(7);
+print_square(7);
+print_triangle(8);
+
 }
