289518ec54c85a421e84669baacbb8cccce4f38b
Leul Assaminew <leulassaminew@gmail.com>
2021-09-02 15:18:38 -0400

    updated

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * mul - multiplies two integers
- * @a: int to be multiplied to b
- * @b: int to be multiplied to a
- * Return: the result of the operation
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
 void print_numbers(void)
 {
 int i;
-for(i = 0;i <= 9;i++){
+
+for (i = 0; i < 10; i++)
+{
 _putchar(i + '0');
 }
+
 _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,7 @@ int _putchar(char c);
 int _islower(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
 
 
