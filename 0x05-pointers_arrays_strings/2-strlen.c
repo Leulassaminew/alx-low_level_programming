d310abd01b8ebc7658590ffe24198f73da4d0eaa
Leul Assaminew <leulassaminew@gmail.com>
2021-09-06 07:45:50 -0400

    updated

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 
 /**
- *  swap_int- swap a and b,
+ * _strlen- swap a and b,
  * @s:pointer to first value
  */
 int _strlen(char *s)
 {
 int i;
 for (i = 0; s[i] != '\0'; ++i);
-return(i);
+return (i);
 }
