e8f2c50329fbb06898fc3fdf51cb56e08dc75add
Leul Assaminew <leulassaminew@gmail.com>
2021-09-06 10:48:21 -0400

    updated

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,16 @@ int _putchar(char c);
 /**
  *  _puts- resets n to 98,
  * @str:address of n
+ * @c:string to print
+ *_putchar:print
+ *return:c
  */
 int _putchar(char c);
 void _puts(char *str)
 {
 int i;
 
-for (i = 0 ;str[i] != '\0' ; i++)
+for (i = 0 ; str[i] != '\0' ; i++)
 {
 _putchar(str[i]);
 }
