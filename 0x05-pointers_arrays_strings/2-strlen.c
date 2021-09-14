46bdd8c6f1736fcdc911566d5c90b962b3b4933b
Leul Assaminew <leulassaminew@gmail.com>
2021-09-14 04:44:04 -0400

    updated

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,15 @@
+#include "main.h"
 
 /**
- * _strlen- swap a and b,
- * @s:pointer to first value
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _strlen(char *s)
 {
 int i;
 
 for (i = 0; s[i] != '\0'; ++i);
-
 return (i);
+
 }
