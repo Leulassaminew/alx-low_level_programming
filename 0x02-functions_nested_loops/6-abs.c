1da183d19d9dd13e5666dd8a7033f3181e3e02d2
Leulassaminew <88404832+Leulassaminew@users.noreply.github.com>
2021-09-19 07:18:23 -0400

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *  _abs - check the code
+ *
+ * Return: int.
+ * @n: arg.
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (n);
+}
+}
