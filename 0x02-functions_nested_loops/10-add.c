83a99b51f3c4f6180dc28220a5c59203b52c4a22
Leulassaminew <88404832+Leulassaminew@users.noreply.github.com>
2021-09-19 07:42:52 -0400

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - check the code
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+int result;
+
+result = a + b;
+return (result);
+}
