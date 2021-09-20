9a23b1b162f52fa7299dacdfceb2ac911e974482
Leulassaminew <Leulassaminew@gmail.com>
2021-09-20 10:58:52 +0000

    Done with the demo app

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,30 @@
+#include "main.h"
 
 /**
- *  *_strcat- resets n to 98,
- * @dest:argument 1
- * @src:argument 2
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
-printf("%s%s", dest, src);
+int i, j;
+
+i = 0;
+j = 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+
+i++;
+															}
+dest[i] = '\0';
+
+return (dest);
 }
