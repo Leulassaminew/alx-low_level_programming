0d9c0284b89169c82dcefe45fc2eed4f95e4a837
Leulassaminew <Leulassaminew@gmail.com>
2021-09-20 07:42:57 +0000

    Done with the demo app

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts- resets n to 98,
+ * @str:address of n
+ */
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0 ; str[i] != '\0' ; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
+
